efe49c7eb232c844d70e082f6367cf5c119fe9cb
akash plackal <akashplackal@gmail.com>
2022-10-07 10:29:05 -0400

    added active states

--- components/Tabitems.js
+++ components/Tabitems.js
@@ -1,17 +1,26 @@
 import { Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Tabitems = ({ item }) => {
+  const r = useRouter();
+  const { keyword } = r.query;
+
   return (
     <Link
       key={item.name}
-      href="/section/[keyword]"
+      // href="/section/[keyword]"
+      href={item.word === "design" ? "/" : "/section/[keyword]"}
       as={`/section/${item.word}`}
     >
       <Box minW="4rem" display="flex" alignItems="center" flexDir="column">
         <Image src={`/${item.word}.jpg`} alt="houses" height="30" width="30" />
-        <Text fontSize="xs" className="open" color="black" fontWeight="600">
+        <Text
+          fontSize="xs"
+          className={keyword === item.word ? "" : "open"}
+          fontWeight="600"
+        >
           {item.name}
         </Text>
       </Box>
